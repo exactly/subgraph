c42f994b3b381952161142db0c358ca89647d238
danilo neves cruz <cruzdanilo@gmail.com>
2022-05-24 12:01:25 -0300

    add timestamp to all entities

--- src/FixedLender.ts
+++ src/FixedLender.ts
@@ -23,6 +23,7 @@ import toId from './utils/toId';
 
 export function handleDeposit(event: DepositEvent): void {
   let entity = new Deposit(toId(event));
+  entity.timestamp = event.block.timestamp;
   entity.market = event.address;
   entity.caller = event.params.caller;
   entity.owner = event.params.owner;
@@ -33,6 +34,7 @@ export function handleDeposit(event: DepositEvent): void {
 
 export function handleWithdraw(event: WithdrawEvent): void {
   let entity = new Withdraw(toId(event));
+  entity.timestamp = event.block.timestamp;
   entity.market = event.address;
   entity.caller = event.params.caller;
   entity.receiver = event.params.receiver;
@@ -44,6 +46,7 @@ export function handleWithdraw(event: WithdrawEvent): void {
 
 export function handleTransfer(event: TransferEvent): void {
   let entity = new Transfer(toId(event));
+  entity.timestamp = event.block.timestamp;
   entity.market = event.address;
   entity.from = event.params.from;
   entity.to = event.params.to;
@@ -53,6 +56,7 @@ export function handleTransfer(event: TransferEvent): void {
 
 export function handleDepositAtMaturity(event: DepositAtMaturityEvent): void {
   let entity = new DepositAtMaturity(toId(event));
+  entity.timestamp = event.block.timestamp;
   entity.market = event.address;
   entity.maturity = event.params.maturity;
   entity.caller = event.params.caller;
@@ -64,6 +68,7 @@ export function handleDepositAtMaturity(event: DepositAtMaturityEvent): void {
 
 export function handleWithdrawAtMaturity(event: WithdrawAtMaturityEvent): void {
   let entity = new WithdrawAtMaturity(toId(event));
+  entity.timestamp = event.block.timestamp;
   entity.market = event.address;
   entity.maturity = event.params.maturity;
   entity.caller = event.params.caller;
@@ -76,6 +81,7 @@ export function handleWithdrawAtMaturity(event: WithdrawAtMaturityEvent): void {
 
 export function handleBorrowAtMaturity(event: BorrowAtMaturityEvent): void {
   let entity = new BorrowAtMaturity(toId(event));
+  entity.timestamp = event.block.timestamp;
   entity.market = event.address;
   entity.maturity = event.params.maturity;
   entity.caller = event.params.caller;
@@ -88,6 +94,7 @@ export function handleBorrowAtMaturity(event: BorrowAtMaturityEvent): void {
 
 export function handleRepayAtMaturity(event: RepayAtMaturityEvent): void {
   let entity = new RepayAtMaturity(toId(event));
+  entity.timestamp = event.block.timestamp;
   entity.market = event.address;
   entity.maturity = event.params.maturity;
   entity.caller = event.params.caller;
@@ -99,6 +106,7 @@ export function handleRepayAtMaturity(event: RepayAtMaturityEvent): void {
 
 export function handleLiquidateBorrow(event: LiquidateBorrowEvent): void {
   let entity = new LiquidateBorrow(toId(event));
+  entity.timestamp = event.block.timestamp;
   entity.market = event.address;
   entity.receiver = event.params.receiver;
   entity.borrower = event.params.borrower;
@@ -110,6 +118,7 @@ export function handleLiquidateBorrow(event: LiquidateBorrowEvent): void {
 
 export function handleAssetSeized(event: AssetSeizedEvent): void {
   let entity = new AssetSeized(toId(event));
+  entity.timestamp = event.block.timestamp;
   entity.market = event.address;
   entity.liquidator = event.params.liquidator;
   entity.borrower = event.params.borrower;
