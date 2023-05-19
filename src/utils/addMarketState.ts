e8b542803128756f143a7a48710a095b6b16f0e5
sebipap <sebipaps@gmail.com>
2023-05-19 10:23:06 -0300

    ♻️ make Market properties not-nullable

--- src/utils/addMarketState.ts
+++ src/utils/addMarketState.ts
@@ -3,14 +3,15 @@ import { FixedPoolState, Market, MarketState } from '../../generated/schema';
 import toId from './toId';
 import { Market as MarketContract } from '../../generated/MarketWETH/Market';
 
-export function updateMarketState(event: ethereum.Event, market: Market): void {
+const INTERVAL = 60 * 60 * 24 * 7 * 4;
+
+function addMarketState(event: ethereum.Event, market: Market): void {
   const marketState = new MarketState(toId(event));
 
   marketState.market = market.id;
   marketState.timestamp = event.block.timestamp.toU32();
-  marketState.floatingDepositShares = market.floatingDepositShares;
+  marketState.totalFloatingBorrowShares = market.totalFloatingBorrowShares;
   marketState.floatingAssets = market.floatingAssets;
-  marketState.floatingBorrowShares = market.floatingBorrowShares;
   marketState.floatingDebt = market.floatingDebt;
   marketState.earningsAccumulator = market.earningsAccumulator;
   marketState.floatingUtilization = market.floatingUtilization;
@@ -21,31 +22,26 @@ export function updateMarketState(event: ethereum.Event, market: Market): void {
   marketState.earningsAccumulatorSmoothFactor = market.earningsAccumulatorSmoothFactor;
   marketState.treasuryFeeRate = market.treasuryFeeRate;
   marketState.totalSupply = market.totalSupply;
+  marketState.lastFloatingDebtUpdate = market.lastFloatingDebtUpdate;
 
   const instance = MarketContract.bind(Address.fromString(market.id));
-
   const timestamp = event.block.timestamp.toI32();
-
-  const INTERVAL = 60 * 60 * 24 * 7 * 4;
   const maxFuturePools = instance.maxFuturePools();
-
   const minMaturity = timestamp - (timestamp % INTERVAL) + INTERVAL;
 
   for (let i = 0; i < maxFuturePools; i += 1) {
     const fixedPoolInstance = instance.fixedPools(BigInt.fromI32(minMaturity + INTERVAL * i));
-
     const fixedPoolState = new FixedPoolState(`${toId(event)}-${i}`);
     fixedPoolState.unassignedEarnings = fixedPoolInstance.getUnassignedEarnings();
     fixedPoolState.borrowed = fixedPoolInstance.getBorrowed();
     fixedPoolState.supplied = fixedPoolInstance.getSupplied();
-    fixedPoolState.unassignedEarnings = fixedPoolInstance.getUnassignedEarnings();
-    fixedPoolState.lastAccrual = fixedPoolInstance.getLastAccrual();
+    fixedPoolState.lastAccrual = fixedPoolInstance.getLastAccrual().toU32();
     fixedPoolState.marketState = marketState.id;
     fixedPoolState.maturity = minMaturity + INTERVAL * i;
-    fixedPoolState.timestamp = event.block.timestamp.toI32();
-
+    fixedPoolState.timestamp = timestamp;
     fixedPoolState.save();
   }
-
   marketState.save();
 }
+
+export default addMarketState;
